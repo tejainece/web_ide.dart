a2decbd8a1b5f81b97b450a2859e0664f5404cac
Ravi Teja Gudapati <tejainece@gmail.com>
2015-04-03 14:58:30 +0200

    Removed mirrors dependence

--- lib/utils/binding.dart
+++ lib/utils/binding.dart
@@ -1,15 +1,7 @@
 part of dockable_utils;
 
 dynamic getModelForItem(item) {
-  dynamic ret = nodeBind(item).templateInstance.model;
-
-  InstanceMirror im = reflect(ret);
-
-  if (im.type.simpleName.toString() == 'Symbol("_GlobalsScope")') {
-    ret = ret.model;
-  } else if (im.type.simpleName.toString() == 'Symbol("_LocalVariableScope")') {
-    //TODO:
-  }
+  dynamic ret = nodeBind(item).templateInstance.model.model;
 
   return ret;
 }
